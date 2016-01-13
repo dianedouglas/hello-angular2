48261d333c8258a23d3af2e12a7892cec27e06d3
Diane Douglas <diane.douglas1@gmail.com>
2016-01-13 14:45:54 -0800

    basic angular 2 framework

---
+++ app/app.component.js
@@ -0,0 +1,10 @@
+(function(app) {
+  app.AppComponent =
+    ng.core.Component({
+      selector: 'diane',
+      template: '<img src="http://static.comicvine.com/uploads/original/12/128825/2510063-thor.jpg" alt="" />'
+    })
+    .Class({
+      constructor: function(){}
+    });
+})(window.app || (window.app = {}));
